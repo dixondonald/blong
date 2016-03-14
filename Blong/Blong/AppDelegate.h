b9bcf931cb008d4b89f1d8a5cef2479c3e2cf300
Donald Dixon <dixondonald@gmail.com>
2016-03-14 13:07:50 -0400

    initial commit

---
+++ Blong/Blong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blong
+//
+//  Created by Ziggy on 3/14/16.
+//  Copyright © 2016 Ziggy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
