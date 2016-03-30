344f5250bf2fa8910739fed16327a55cbd8cef19
Donald Dixon <dixondonald@gmail.com>
2016-03-30 13:52:53 -0400

    background and timer completed

--- Blong/Blong/ViewController.m
+++ Blong/Blong/ViewController.m
@@ -24,6 +24,8 @@
 @property (nonatomic, strong) UIDynamicItemBehavior *rightPaddleBehavior;
 @property (nonatomic, strong) UIDynamicItemBehavior *ballBehavior;
 
+@property (nonatomic, strong) UILabel *countLabel;
+@property NSInteger count;
 @end
 
 @implementation ViewController
@@ -32,21 +34,33 @@
     [super viewDidLoad];
     self.animator = [[UIDynamicAnimator new] initWithReferenceView:self.view];
     
+    UIImageView *bgView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"pongbg"]];
+    bgView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view addSubview:bgView];
    
+    
+    CGRect countRect = CGRectMake(self.view.frame.size.width / 2 - 10, self.view.frame.size.height / 2 - 10, 50, 50);
+    self.countLabel = [[UILabel alloc] initWithFrame:countRect];
+    self.countLabel.textColor = [UIColor whiteColor];
+    [[self countLabel] setFont:[UIFont fontWithName:@"Futura-Medium" size:40]];
+
+    [self.view addSubview:self.countLabel];
+
+    
     [self createBall];
     [self pushBall];
     
     
     CGRect leftPaddleRect = CGRectMake(10, self.view.bounds.size.height / 2, 40, 60);
     self.leftPaddleView = [[UIView alloc] initWithFrame:leftPaddleRect];
-    self.leftPaddleView.backgroundColor = [UIColor blackColor];
+    self.leftPaddleView.backgroundColor = [UIColor whiteColor];
     self.leftPaddleView.layer.cornerRadius = 5;
     [self.view addSubview:self.leftPaddleView];
     
     
     CGRect rightPaddleRect = CGRectMake(self.view.bounds.size.width - 50, self.view.bounds.size.height / 2, 40, 60);
     self.rightPaddleView = [[UIView alloc] initWithFrame:rightPaddleRect];
-    self.rightPaddleView.backgroundColor = [UIColor blackColor];
+    self.rightPaddleView.backgroundColor = [UIColor whiteColor];
     self.rightPaddleView.layer.cornerRadius = 5;
     [self.view addSubview:self.rightPaddleView];
     
@@ -98,7 +112,7 @@
 - (void)createBall {
     CGRect ballRect = CGRectMake(self.view.bounds.size.width / 2, self.view.bounds.size.height / 2, 16, 16);
     self.ballView = [[UIView alloc] initWithFrame:ballRect];
-    self.ballView.backgroundColor = [UIColor blackColor];
+    self.ballView.backgroundColor = [UIColor whiteColor];
     self.ballView.layer.cornerRadius = 8;
     [self.view addSubview:self.ballView];
     
@@ -127,20 +141,33 @@
     [self.animator addBehavior:self.pushBehavior];
 }
 
+- (void) startTimer {
+    self.count = 4;
+    [NSTimer scheduledTimerWithTimeInterval:.5 target:self selector:@selector(countdownTimer:) userInfo:nil repeats:YES];
+}
+
+-(void) countdownTimer:(NSTimer*)timer{
+
+    self.count--;
+    self.countLabel.text = [NSString stringWithFormat:@"%ld", (long)self.count];
+        NSLog(@"%ld", (long)self.count);
+        if (self.count == 0) {
+            NSLog(@"go");
+            self.countLabel.text = @"";
+            [timer invalidate];
+            [self createBall];
+            [self pushBall];
+        
+    }
+}
+
 - (void)collisionBehavior:(UICollisionBehavior *)behavior beganContactForItem:(id<UIDynamicItem>)item
    withBoundaryIdentifier:(id<NSCopying>)identifier atPoint:(CGPoint)p {
    NSString *boundary = (NSString *)identifier;
-    if ([boundary isEqualToString:@"right"]) {
+    if ([boundary isEqualToString:@"right"] || [boundary isEqualToString:@"left"]) {
         [self removeBall];
-        [self createBall];
-        [self pushBall];
+        [self startTimer];
     }
-    else if ([boundary isEqualToString:@"left"]) {
-        [self removeBall];
-        [self createBall];
-        [self pushBall];
-    }
-
 }
 
 - (void)didReceiveMemoryWarning {
