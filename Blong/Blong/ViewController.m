31383c0d948bcb0b284c88b94e491d7afab187d7
Donald Dixon <dixondonald@gmail.com>
2016-04-25 17:03:24 -0400

    in-app purchase completed

--- Blong/Blong/ViewController.m
+++ Blong/Blong/ViewController.m
@@ -10,8 +10,10 @@
 #import <AVFoundation/AVFoundation.h>
 #import <GameKit/GameKit.h>
 @import GoogleMobileAds;
+#import <StoreKit/StoreKit.h>
 
-@interface ViewController () <UICollisionBehaviorDelegate, GKGameCenterControllerDelegate, GADInterstitialDelegate>
+
+@interface ViewController () <UICollisionBehaviorDelegate, GKGameCenterControllerDelegate, GADInterstitialDelegate, SKProductsRequestDelegate, SKPaymentTransactionObserver>
 
 -(void)authenticateLocalPlayer;
 -(void)reportScore;
@@ -72,8 +74,9 @@
 @property (nonatomic, strong) UIButton *hardestButton;
 @property (nonatomic, strong) UIButton *gcButton;
 @property (nonatomic, strong) UIButton *IAPButton;
+@property (nonatomic, strong) UIButton *restoreButton;
 
-
+@property BOOL areAdsRemoved;
 @property BOOL isBall;
 @property BOOL isExtraBall;
 @property BOOL isThirdBall;
@@ -101,6 +104,9 @@
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     self.gameCount = [defaults integerForKey:@"HighScore"];
 
+    self.areAdsRemoved = [[NSUserDefaults standardUserDefaults] boolForKey:@"areAdsRemoved"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
     self.animator = [[UIDynamicAnimator new] initWithReferenceView:self.view];
     
     self.angles = [[NSArray alloc] initWithObjects:@1.0f, @2.5f, @4.0f, @5.35f, @7.0f, @8.5f, @10.15f, @11.75f, nil];
@@ -153,7 +159,6 @@
 
 - (void)interstitialDidDismissScreen:(GADInterstitial *)interstitial {
     self.interstitial = [self createAndLoadInterstitial];
-    self.gameCount = 0;
 }
 
 - (void)displayAd {
@@ -169,6 +174,122 @@
     [self createPaddles];
     [self startTimer];
 }
+
+#define kRemoveAdsProductIdentifier @"pongulator_remove_ads"
+
+- (void)removeAds:(UIButton*)sender {
+    NSLog(@"User requests to remove ads");
+    
+    if([SKPaymentQueue canMakePayments]){
+        NSLog(@"User can make payments");
+        
+        SKProductsRequest *productsRequest = [[SKProductsRequest alloc] initWithProductIdentifiers:[NSSet setWithObject:kRemoveAdsProductIdentifier]];
+        productsRequest.delegate = self;
+        [productsRequest start];
+        
+    }
+    else{
+        NSLog(@"User cannot make payments due to parental controls");
+    }
+}
+
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response{
+    SKProduct *validProduct = nil;
+    NSUInteger count = [response.products count];
+    if(count > 0){
+        validProduct = [response.products objectAtIndex:0];
+        NSLog(@"Products Available!");
+        [self purchase:validProduct];
+    }
+    else if(!validProduct){
+        NSLog(@"No products available");
+        //this is called if your product id is not valid, this shouldn't be called unless that happens.
+    }
+}
+
+- (void)purchase:(SKProduct *)product{
+    SKPayment *payment = [SKPayment paymentWithProduct:product];
+    
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
+}
+
+- (void)restorePurchase:(UIButton*)sender {
+    //this is called when the user restores purchases, you should hook this up to a button
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    [[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+}
+
+
+- (void) paymentQueueRestoreCompletedTransactionsFinished:(SKPaymentQueue *)queue
+{
+    NSLog(@"received restored transactions: %lu", (unsigned long)queue.transactions.count);
+    for(SKPaymentTransaction *transaction in queue.transactions){
+        if(transaction.transactionState == SKPaymentTransactionStateRestored){
+            //called when the user successfully restores a purchase
+            NSLog(@"Transaction state -> Restored");
+            
+            //if you have more than one in-app purchase product,
+            //you restore the correct product for the identifier.
+            //For example, you could use
+            //if(productID == kRemoveAdsProductIdentifier)
+            //to get the product identifier for the
+            //restored purchases, you can use
+            //
+            //NSString *productID = transaction.payment.productIdentifier;
+            [self doRemoveAds];
+            [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+            break;
+        }
+    }   
+}
+
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions{
+    for(SKPaymentTransaction *transaction in transactions){
+        switch(transaction.transactionState){
+            case SKPaymentTransactionStatePurchasing: NSLog(@"Transaction state -> Purchasing");
+                //called when the user is in the process of purchasing, do not add any of your own code here.
+                break;
+            case SKPaymentTransactionStatePurchased:
+                //this is called when the user has successfully purchased the package (Cha-Ching!)
+                [self doRemoveAds]; //you can add your code for what you want to happen when the user buys the purchase here, for this tutorial we use removing ads
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                NSLog(@"Transaction state -> Purchased");
+                break;
+            case SKPaymentTransactionStateRestored:
+                NSLog(@"Transaction state -> Restored");
+                //add the same code as you did from SKPaymentTransactionStatePurchased here
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                //called when the transaction does not finish
+                if(transaction.error.code == SKErrorPaymentCancelled){
+                    NSLog(@"Transaction state -> Cancelled");
+                    //the user cancelled the payment ;(
+                }
+                [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+                break;
+        }
+    }
+}
+
+- (void)doRemoveAds{
+    self.areAdsRemoved = YES;
+    self.IAPButton.hidden = YES;
+    self.IAPButton.enabled = NO;
+    self.restoreButton.hidden = YES;
+    self.restoreButton.enabled = NO;
+    [[NSUserDefaults standardUserDefaults] setBool:self.areAdsRemoved forKey:@"areAdsRemoved"];
+    //use NSUserDefaults so that you can load whether or not they bought it
+    //it would be better to use KeyChain access, or something more secure
+    //to store the user data, because NSUserDefaults can be changed.
+    //You're average downloader won't be able to change it very easily, but
+    //it's still best to use something more secure than NSUserDefaults.
+    //For the purpose of this tutorial, though, we're going to use NSUserDefaults
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+
 
 - (void)gcOpen:(UIButton*)sender {
     [self showLeaderboardAndAchievements:YES];
@@ -394,18 +515,42 @@
     self.gcButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:25];
     [self.view addSubview:self.gcButton];
     
-    CGRect IAPRect = CGRectMake(2, self.view.frame.size.height - 28, 120, 25);
+
+    CGRect IAPRect = CGRectMake(2, self.view.frame.size.height - 48, 120, 25);
     self.IAPButton = [[UIButton alloc] initWithFrame:IAPRect];
     [self.IAPButton addTarget:self
-                      action:@selector(gcOpen:)
-            forControlEvents:UIControlEventTouchUpInside];
+                       action:@selector(removeAds:)
+             forControlEvents:UIControlEventTouchUpInside];
     //    [self.IAPButton setBackgroundColor:[UIColor colorWithWhite:1 alpha:.5]];
     //    [self.IAPButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [self.IAPButton.titleLabel setTextAlignment:NSTextAlignmentLeft];
     [self.IAPButton setTitle:@"remove ads" forState:UIControlStateNormal];
     self.IAPButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:25];
     [self.view addSubview:self.IAPButton];
+    
+    if (self.areAdsRemoved) {
+        self.IAPButton.hidden = YES;
+        self.IAPButton.enabled = NO;
+    }
+    
 
+    CGRect restoreRect = CGRectMake(2, self.view.frame.size.height - 23, 138, 20);
+    self.restoreButton = [[UIButton alloc] initWithFrame:restoreRect];
+    [self.restoreButton addTarget:self
+                       action:@selector(restorePurchase:)
+             forControlEvents:UIControlEventTouchUpInside];
+    //    [self.IAPButton setBackgroundColor:[UIColor colorWithWhite:1 alpha:.5]];
+    //    [self.IAPButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.restoreButton.titleLabel setTextAlignment:NSTextAlignmentLeft];
+//    [self.restoreButton.titleLabel setLineBreakMode:NSLineBreakByCharWrapping];
+    [self.restoreButton setTitle:@"restore purchase" forState:UIControlStateNormal];
+    self.restoreButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue-UltraLight" size:20];
+    [self.view addSubview:self.restoreButton];
+
+    if (self.areAdsRemoved) {
+        self.restoreButton.hidden = YES;
+        self.restoreButton.enabled = NO;
+    }
     
     CGRect titleRect = CGRectMake(self.view.frame.size.width / 2 - 250, 10, 500, 75);
     self.titleLabel = [[UILabel alloc] initWithFrame:titleRect];
@@ -421,7 +566,7 @@
 }
 
 -(void)removeMenu {
-    
+    [self.restoreButton removeFromSuperview];
     [self.IAPButton removeFromSuperview];
     [self.gcButton removeFromSuperview];
     [self.titleLabel removeFromSuperview];
@@ -650,10 +795,13 @@
                 [defaults setInteger:self.gameCount forKey:@"HighScore"];
                 [defaults synchronize];
                 
-                if (self.gameCount == 5) {
-                    [self displayAd];
+                if (!self.areAdsRemoved) {
+                    if (self.gameCount >= 5) {
+                        [self displayAd];
+                        self.gameCount = 0;
+                    }
                 }
-
+                
                 [self retryMenu];
             }
         }
@@ -678,8 +826,11 @@
                 [defaults setInteger:self.gameCount forKey:@"HighScore"];
                 [defaults synchronize];
                 
-                if (self.gameCount == 5) {
-                    [self displayAd];
+                if (!self.areAdsRemoved) {
+                    if (self.gameCount >= 5) {
+                        [self displayAd];
+                        self.gameCount = 0;
+                    }
                 }
                 
                 [self retryMenu];
@@ -706,10 +857,13 @@
                 [defaults setInteger:self.gameCount forKey:@"HighScore"];
                 [defaults synchronize];
                 
-                if (self.gameCount == 5) {
-                    [self displayAd];
+                if (!self.areAdsRemoved) {
+                    if (self.gameCount >= 5) {
+                        [self displayAd];
+                        self.gameCount = 0;
+                    }
                 }
-                
+            
                 [self retryMenu];
             }
         }
